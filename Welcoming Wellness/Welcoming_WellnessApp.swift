ad9784aaf0a182de30bb0da0c1be9f10cf1949cb
Jason Wong <jasonlwong25@gmail.com>
2023-04-22 13:12:39 -0700

    Initial Commit

---
+++ Welcoming Wellness/Welcoming_WellnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Welcoming_WellnessApp.swift
+//  Welcoming Wellness
+//
+//  Created by Jason Wong on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Welcoming_WellnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
