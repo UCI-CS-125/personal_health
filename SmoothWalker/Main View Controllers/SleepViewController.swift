567409297a641fec7e6ca1019438b3b66e667bd5
Jason Wong <jasonlwong25@gmail.com>
2023-05-10 23:06:57 -0700

    Added icons and sleepviewcontrollerfile

---
+++ SmoothWalker/Main View Controllers/SleepViewController.swift
@@ -0,0 +1,61 @@
+
+//
+//  SleepViewController.swift
+//  SmoothWalker
+//
+//  Created by Ben Landry on 5/2/23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+import Charts
+class SleepViewController: UIViewController {
+    @available(iOS 16.0, *)
+    struct Viewsy: View {
+        let currentWeek: [HoursSlept] = [
+            HoursSlept(day: "20220716", hours: 6),
+            HoursSlept(day: "20220717", hours: 6),
+            HoursSlept(day: "20220718", hours: 7),
+            HoursSlept(day: "20220719", hours: 4),
+            HoursSlept(day: "20220720", hours: 5),
+            HoursSlept(day: "20220721", hours: 5),
+            HoursSlept(day: "20220722", hours: 8),
+            HoursSlept(day: "20220723", hours: 9)
+        ]
+        
+        var body: some View {
+            VStack {
+                GroupBox ( "Hours of Sleep") {
+                    Chart(currentWeek) {
+                        BarMark(
+                            x: .value("Week Day", $0.theDate, unit: .day),
+                            y: .value("Hours of Sleep", $0.hours)
+                        )
+                    }
+                }
+                .frame(height: 500)
+                
+                Spacer()
+            }
+            .padding()
+        }
+    }
+    
+    
+    
+    @available(iOS 16.0, *)
+    struct HomePreviews: PreviewProvider{
+        static var previews: some View{
+            Viewsy()
+        }
+    }
+        
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = tabBarItem.title
+        view.backgroundColor = .purple
+    }
+
+}
