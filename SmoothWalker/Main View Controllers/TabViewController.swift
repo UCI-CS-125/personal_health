9f39a0de013125a9e40ad97cc98fad633a31910f
Jason Wong <jasonlwong25@gmail.com>
2023-05-13 16:56:07 -0700

    Created Permission ViewController and Tab View Controller

---
+++ SmoothWalker/Main View Controllers/TabViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TabViewController.swift
+//  SmoothWalker
+//
+//  Created by Jason Wong on 5/11/23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TabViewController: UITabBarController {
+
+            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(true)
+            navigationController?.setNavigationBarHidden(true, animated: animated)
+        print("hello tab")
+
+
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
