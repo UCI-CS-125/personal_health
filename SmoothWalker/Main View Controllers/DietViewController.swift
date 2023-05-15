a388ef4ec265db3ed125e5ba5963faf4ad73ee75
Serena Rupani <ssrupani@uci.edu>
2023-05-14 17:00:30 -0700

    Added DietViewController/Storyboard and names for tab bar items

---
+++ SmoothWalker/Main View Controllers/DietViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DietViewController.swift
+//  SmoothWalker
+//
+//  Created by Serena Rupani on 5/14/23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import UIKit
+
+class DietViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
+
