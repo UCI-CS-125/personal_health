290f649ff93b8904059442cd44b11fbbccd13652
uliazaman02 <ulia.zaman02@gmail.com>
2023-06-04 23:52:02 -0700

    Added Diet Data to firestore

--- WelcomingWellness/Main View Controllers/DietViewController.swift
+++ WelcomingWellness/Main View Controllers/DietViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class DietViewController: UIViewController, UITextFieldDelegate {
     
@@ -16,7 +18,26 @@ class DietViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var dinner: UITextField!
     
+    @IBOutlet weak var saveButton: UIButton!
     
+    
+    var docRef: DocumentReference!
+    
+    @IBAction func saveTapped(_sender: UIButton!) {
+        guard let breakfastText = breakfast.text, !breakfastText.isEmpty else {return}
+        guard let lunchText = lunch.text, !lunchText.isEmpty else {return}
+        guard let dinnerText = dinner.text, !dinnerText.isEmpty else {return}
+        let dataToSave: [String: Any] = ["breakfast": breakfastText, "lunch": lunchText, "dinner": dinnerText]
+        docRef.setData(dataToSave) { (error) in
+            if let error = error {
+                print("errror: \(error.localizedDescription)")
+            } else {
+                print("Diet Data has been saved")
+            }
+        }
+    }
+    
+
 
     
     override func viewDidLoad() {
@@ -25,7 +46,29 @@ class DietViewController: UIViewController, UITextFieldDelegate {
         lunch.delegate = self
         dinner.delegate = self
         
+        docRef = Firestore.firestore().document("dietData/meals")
+
     }
+    
+
+    @IBAction func buttonClicked(_ sender: Any) {
+        print("Button tapped")
+        guard let breakfastText = breakfast.text, !breakfastText.isEmpty else {return}
+        guard let lunchText = lunch.text, !lunchText.isEmpty else {return}
+        guard let dinnerText = dinner.text, !dinnerText.isEmpty else {return}
+        let dataToSave: [String: Any] = ["breakfast": breakfastText, "lunch": lunchText, "dinner": dinnerText]
+        docRef.setData(dataToSave) { (error) in
+            if let error = error {
+                print("errror: \(error.localizedDescription)")
+            } else {
+                print("Diet Data has been saved")
+            }
+        }
+    }
+    
+//    func buttonAction(sender: UIButton!) {
+//      print("Button tapped")
+//    }
     
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
