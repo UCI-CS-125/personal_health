ad42e0adf01dbd293025f2d70dfc69b8b60c6a20
uliazaman02 <ulia.zaman02@gmail.com>
2023-06-05 18:49:07 -0700

    Store dietData as array and updated Diet UI

--- WelcomingWellness/Main View Controllers/DietViewController.swift
+++ WelcomingWellness/Main View Controllers/DietViewController.swift
@@ -12,39 +12,29 @@ import FirebaseFirestore
 
 class DietViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var breakfast: UITextField!
+    @IBOutlet weak var fruit: UITextField!
     
-    @IBOutlet weak var lunch: UITextField!
+    @IBOutlet weak var vegetable: UITextField!
     
-    @IBOutlet weak var dinner: UITextField!
+    @IBOutlet weak var grain: UITextField!
+    
+    @IBOutlet weak var protein: UITextField!
+    
+    @IBOutlet weak var dairy: UITextField!
     
     @IBOutlet weak var saveButton: UIButton!
     
-    
     var docRef: DocumentReference!
-    
-    @IBAction func saveTapped(_sender: UIButton!) {
-        guard let breakfastText = breakfast.text, !breakfastText.isEmpty else {return}
-        guard let lunchText = lunch.text, !lunchText.isEmpty else {return}
-        guard let dinnerText = dinner.text, !dinnerText.isEmpty else {return}
-        let dataToSave: [String: Any] = ["breakfast": breakfastText, "lunch": lunchText, "dinner": dinnerText]
-        docRef.setData(dataToSave) { (error) in
-            if let error = error {
-                print("errror: \(error.localizedDescription)")
-            } else {
-                print("Diet Data has been saved")
-            }
-        }
-    }
-    
 
-
-    
+    let db = Firestore.firestore()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        breakfast.delegate = self
-        lunch.delegate = self
-        dinner.delegate = self
+        fruit.delegate = self
+        vegetable.delegate = self
+        grain.delegate = self
+        protein.delegate = self
+        dairy.delegate = self
         
         docRef = Firestore.firestore().document("dietData/meals")
 
@@ -53,22 +43,29 @@ class DietViewController: UIViewController, UITextFieldDelegate {
 
     @IBAction func buttonClicked(_ sender: Any) {
         print("Button tapped")
-        guard let breakfastText = breakfast.text, !breakfastText.isEmpty else {return}
-        guard let lunchText = lunch.text, !lunchText.isEmpty else {return}
-        guard let dinnerText = dinner.text, !dinnerText.isEmpty else {return}
-        let dataToSave: [String: Any] = ["breakfast": breakfastText, "lunch": lunchText, "dinner": dinnerText]
-        docRef.setData(dataToSave) { (error) in
-            if let error = error {
-                print("errror: \(error.localizedDescription)")
-            } else {
-                print("Diet Data has been saved")
-            }
-        }
+        
+        // Get previously stored array from firebase
+        let dietArray = db.collection("dietData").document("meals")
+        
+        guard let fruitCal = fruit.text, !fruitCal.isEmpty else {return}
+        guard let vegetableCal = vegetable.text, !vegetableCal.isEmpty else {return}
+        guard let grainCal = grain.text, !grainCal.isEmpty else {return}
+        guard let proteinCal = protein.text, !proteinCal.isEmpty else {return}
+        guard let dairyCal = dairy.text, !dairyCal.isEmpty else {return}
+        
+        dietArray.updateData([
+            "fruits": FieldValue.arrayUnion([Int(fruitCal) ?? 0]), "vegetables": FieldValue.arrayUnion([Int(vegetableCal) ?? 0]), "grains": FieldValue.arrayUnion([Int(grainCal) ?? 0]), "proteins": FieldValue.arrayUnion([Int(proteinCal) ?? 0]), "dairy": FieldValue.arrayUnion([Int(dairyCal) ?? 0])
+        ])
+
+//        let dataToSave: [String: Array] = ["fruits": fruitsArr, "vegetables": vegetablesArr, "grains": grainsArr, "proteins": proteinArr, "dairy": dairyArr]
+//        docRef.setData(dataToSave) { (error) in
+//            if let error = error {
+//                print("errror: \(error.localizedDescription)")
+//            } else {
+//                print("Diet Data has been saved")
+//            }
+//        }
     }
-    
-//    func buttonAction(sender: UIButton!) {
-//      print("Button tapped")
-//    }
     
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
