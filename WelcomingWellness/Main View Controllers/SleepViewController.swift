a11126a7620284d1dcf73d67dc05a75f0c0ae263
Lee Chu <lchu8313@gmail.com>
2023-06-06 19:22:15 -0700

    firebase implemented for sleepview + changed ui

--- WelcomingWellness/Main View Controllers/SleepViewController.swift
+++ WelcomingWellness/Main View Controllers/SleepViewController.swift
@@ -3,58 +3,57 @@
 //  SleepViewController.swift
 //  SmoothWalker
 //
-//  Created by Ben Landry on 5/2/23.
+//  Created by Lee Chu on 5/2/23.
 //  Copyright © 2023 Apple. All rights reserved.
 //
 
 import UIKit
 import SwiftUI
 import Charts
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+import Foundation
+
 class SleepViewController: UIViewController {
-    @available(iOS 16.0, *)
-    struct Viewsy: View {
-        let currentWeek: [HoursSlept] = [
-            HoursSlept(day: "20220716", hours: 6),
-            HoursSlept(day: "20220717", hours: 6),
-            HoursSlept(day: "20220718", hours: 7),
-            HoursSlept(day: "20220719", hours: 4),
-            HoursSlept(day: "20220720", hours: 5),
-            HoursSlept(day: "20220721", hours: 5),
-            HoursSlept(day: "20220722", hours: 8),
-            HoursSlept(day: "20220723", hours: 9)
-        ]
-
-        var body: some View {
-            VStack {
-                GroupBox ( "Hours of Sleep") {
-                    Chart(currentWeek) {
-                        BarMark(
-                            x: .value("Week Day", $0.theDate, unit: .day),
-                            y: .value("Hours of Sleep", $0.hours)
-                        )
-                    }
-                }
-                .frame(height: 500)
-
-                Spacer()
-            }
-            .padding()
-        }
-    }
-
-
-
-    @available(iOS 16.0, *)
-    struct HomePreviews: PreviewProvider{
-        static var previews: some View{
-            Viewsy()
-        }
-    }
-        
     
+    
+    @IBOutlet weak var hoursSleptTextField: UITextField!
+    
+    var docRef : DocumentReference!
+    
+    @IBAction func saver(_ sender: UIButton) {
+        guard let hoursText =  hoursSleptTextField.text, !hoursText.isEmpty else {return}
+        let dataToSave: [String: Any] = ["hours" : hoursText]
+//        docRef.setData(dataToSave) { (error) in
+//            if let error = error {
+//                print("OH NO! Error occurred. Here it is: \(error.localizedDescription)")
+//            } else {
+//                print("Woohoo! Sleep hours have been stored!")
+//            }
+//        }
+        Firestore.firestore().collection("sampleData").document("hoursSleptDoc").setData(dataToSave)
+        { err in
+            if let err = err {
+                print("OH NO! Error occurred. Here it is: \(err.localizedDescription)")
+            } else {
+                print("Woohoo! Sleep hours have been stored!")
+            }
+            
+        }
+        
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         title = tabBarItem.title
+//        docRef = Firestore.firestore().collection("sampleData").document("hoursSleptDoc")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
 }
+
